8c332d1a1a9b0372ab97938336b59e6dd8a81868
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-06-01 02:38:10 -0400

    Made Delete Player Card component

---
+++ src/components/DeletePlayerCard/DeletePlayerCard.js
@@ -0,0 +1,12 @@
+import './DeletePlayerCard.scss';
+import React from 'react';
+import { deleteIcon } from '../../assets/icons/indexIcons';
+
+export default function DeletePlayerCard() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
--- src/components/PlayerCard/PlayerCard.js
+++ src/components/PlayerCard/PlayerCard.js
@@ -1,11 +1,27 @@
+import './PlayerCard.scss';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import edit from '../../assets/icons/indexIcons';
+import DeletePlayerCard from '../DeletePlayerCard/DeletePlayerCard';
 
 
-export default function PlayerCard() {
+export default function PlayerCard({ img, id, playerID, name }) {
     return (
-        <div>
-            
-        </div>
+        <article className='playerCard'>
+            <div className='playerCard-top' style={{ backgroundImage: `url(${img})` }}>
+                <Link to={`/dashboard/1/team/${playerID}/edit`}>
+                    <img className='portfolioCard__icon-edit' src={edit} alt="Edit portfolio card button" />
+                </Link>
+                <DeletePlayerCard
+                    key={playerID}
+                    playerID={playerID}
+                    name={name} />
+            </div>
+            <div className='playerCard-bottom'>
+
+            </div>
+
+        </article>
     );
 }
 
