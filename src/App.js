cd87f64651cc620adfda6776b04483da77088a89
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-06-01 02:49:36 -0400

    Added TeamPageRoute on App.js BrowserRouter

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
 import DashBoardPage from './pages/DashboardPage/DashBoardPage';
+import TeamPage from './pages/TeamPage/TeamPage';
 
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/" exact component={HomePage}/>
         <Route path="/signup" exact component={SignUpPage}/>
         <Route path="/dashboard/:id" exact component={DashBoardPage}/>
+        <Route path='/dashboard/:id/team' exact component={TeamPage}/>
       </Switch>
     </BrowserRouter>
   );
